9976efcf70f5e22784f90ba63ebe2f3f80dfb652
joymogere42-tech <joymogere42@gmail.com>
2026-03-27 09:03:20 +0300

    Delete Day 4 exe 3.py

--- Day 4 exe 3.py
+++
@@ -1,9 +0,0 @@
-# Take inputs from the user
-text = input("Enter a string: ")
-char = input("Enter a character to count: ")
-
-# Count occurrences
-count = text.count(char)
-
-# Print the result
-print(count)
